4eca5c40da476ae670860c93890581a11569b19e
Moataz Khallaf <MoatazKhallaf@outlook.com>
2023-11-06 11:39:39 -0700

    add Navbar and Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// using this as the router
+
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+
+// imports
+import NavBar from "./components/Navbar";
+import Main from "./pages/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<NavBar />
+			<HashRouter>
+				<Routes>
+					<Route exact path="/" element={<Main />} />
+				</Routes>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+// in the case that you make more pages
+// just rename this to homepage or something
+
+import React from "react";
+
+// imports
+import About from "../containers/About";
+import Home from "../containers/Home";
+import Portfolio from "../containers/Portfolio";
+import Contact from "../containers/Contract";
+
+export default function Main() {
+	return <div>yeet</div>;
+}
